35b41d1cd6055c0f230a5db98d33be7f1bdb4004
Nobody Never <x@n3xtgen.net>
2017-04-01 21:21:29 +0200

    Initial Maven archetype

---
+++ src/main/java/net/n3xtgen/App.java
@@ -0,0 +1,13 @@
+package net.n3xtgen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
